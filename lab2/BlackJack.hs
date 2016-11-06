8c1541b468e558c9f3bdd6ea82ea40d033e4c181
emiilax <e.axelsson@hotmail.se>
2016-11-06 14:34:26 +0100

    Added given .hs files

---
+++ lab2/BlackJack.hs
@@ -0,0 +1,8 @@
+-- hand2 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty)
+--
+-- size hand2 = size (Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty))
+--            = 1 + size (Add (Card Jack Spades) Empty)
+--            = 1 + 1 + Empty
+--            = 1 + 1 + 0
+--            = 2
+--
