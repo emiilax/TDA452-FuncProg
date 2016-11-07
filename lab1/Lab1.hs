3d288b2cf562f9db854b4fe622296b6641f333a1
Erik Karlkvist <erikkarlkvist95@gmail.com>
2016-11-07 15:16:33 +0100

    fixed errors from feedback

--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -9,12 +9,12 @@ power n k = n * power n (k-1)
 
 -- 2:
 power1 :: Integer -> Integer -> Integer
-power1 n k | k < 0 = error "power: negative argument"
+power1 n k | k < 0 = error "power1: negative argument"
 power1 n k = product (replicate (fromInteger k) n)
 
 --3:
 power2 :: Integer -> Integer -> Integer
-power2 n k | k < 0 = error "power: negative argument"
+power2 n k | k < 0 = error "power2: negative argument"
 power2 n 0 = 1
 power2 n k | even k = power2 (n*n) (div k 2)
 power2 n k | odd k = n * power2 n (k-1)
@@ -24,10 +24,12 @@ power2 n k | odd k = n * power2 n (k-1)
 --5 is odd number, 10 is even number, 51 is pretty high and 10000 is very high. Power should work for all these (higher than 10000 is very slow)
 --if we have a comprehension list like this we get all types of combinations between these values
 testlist = [(x,y) | x<-[0,-1,1,5,10,51,10000], y<-[0,-1,1,5,10,51,10000]]
+testlist2 = []
 
 --run test by writing "mytest testlist" in terminal
 mytest :: [(Integer, Integer)] -> Bool
-mytest [(i1,i2)] = prop_powers i1 i2
+mytest []          = error "mytest: empty list"
+mytest [(i1,i2)]   = prop_powers i1 i2
 mytest ((i1,i2):l) = prop_powers i1 i2 && mytest l
 
 
