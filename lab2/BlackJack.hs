c02ca43db83d962f78b1edb3afc2da9a41cd85e2
Erik Karlkvist <erikkarlkvist95@gmail.com>
2016-11-07 15:24:43 +0100

    added functions to implement

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -1,3 +1,7 @@
+module BlackJack where
+import Cards
+import RunGame
+
 -- hand2 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty)
 --
 -- size hand2 = size (Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty))
@@ -6,3 +10,11 @@
 --            = 1 + 1 + 0
 --            = 2
 --
+
+--empty :: Hand
+
+--value :: Hand -> Integer
+
+--gameOver :: hand -> Hand -> Player
+
+--winner :: Hand -> Hand -> Player
