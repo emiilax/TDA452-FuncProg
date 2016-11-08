d56f38eec1da8bf458a5e9378e592b1d085c402b
emiilax <e.axelsson@hotmail.se>
2016-11-08 16:37:41 +0100

    valuefunction in progress

--- lab2/EmilBlackJack.hs
+++ lab2/EmilBlackJack.hs
@@ -16,11 +16,23 @@ empty = Empty
 
 rankValue :: Rank -> Integer
 rankValue (Numeric k) = k
+rankValue Ace | nbrOfAces < 2 || valueMinusAces < 10  = 11
+rankValue Ace = 1
 rankValue _ = 10
 
+nbrOfAces :: Hand -> Integer
+nbrOfAces Empty = 0
+nbrOfAces (Add (Card r s) hand) | r == Ace = 1 + nbrOfAces hand
+nbrOfAces (Add (Card r s) hand) = nbrOfAces hand
+
+valueMinusAces :: Hand -> Integer
+valueMinusAces Empty = 0
+valueMinusAces (Add (Card r s) hand) | r == Ace = valueMinusAces hand
+valueMinusAces (Add (Card r s) hand) = rankValue r + valueMinusAces hand
 
 value :: Hand -> Integer
-value Empty = 0 
+value Empty = 0
+--value (Add (Card r s) hand) | r == Ace = value hand
 value (Add (Card r s) hand) = rankValue r + value hand
 
 --Hand hand1 = (Add (Card Ace Spades) Empty)
