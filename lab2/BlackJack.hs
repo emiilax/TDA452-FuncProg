8747d55a18aae47af1a6e824cdf3f9d1cdca3add
Erik Karlkvist <erikkarlkvist95@gmail.com>
2016-11-08 16:22:03 +0100

    Value works

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -15,26 +15,40 @@ empty :: Hand
 empty = Empty
 
 example_card_1 = Card (Numeric 5) Clubs
-example_card_2 = Card Jack Spades
-example_card_3 = Card Ace Spades
+example_card_2 = Card King Spades
+example_card_3 = Card Ace Clubs
+example_card_4 = Card (Numeric 5) Diamonds
 
 example_hand_1 = Add example_card_1 Empty
 example_hand_2 = Add example_card_2 example_hand_1
 example_hand_3 = Add example_card_3 example_hand_2
+example_hand_4 = Add example_card_4 example_hand_3
 
 value :: Hand -> Integer
 value Empty = 0
-value (Add (Card rank suit) hand) = cardValue rank + value hand
+value hand | (nbrOfAces hand) == 1 && value (removeAces hand) <11 = value (removeAces hand) + 11
+           | (nbrOfAces hand) == 1 && value (removeAces hand) >=11 = value (removeAces hand) + nbrOfAces hand
+           | (nbrOfAces hand) > 1 = value (removeAces hand) + nbrOfAces hand
+value (Add card hand) = cardValue card + value hand 
 
-cardValue :: Rank -> Integer
-cardValue (Numeric m)  = m
-cardValue r | r == Ace = 0
-cardValue _            = 10
+--might be unnecessery
+cardValue :: Card -> Integer
+cardValue (Card r s) = rankValue r
+
+removeAces :: Hand -> Hand
+removeAces Empty = Empty
+removeAces (Add (Card r s) hand) | r == Ace = removeAces hand
+removeAces (Add (Card r s) hand) = (Add (Card r s) (removeAces hand))
+
+rankValue :: Rank -> Integer
+rankValue (Numeric m)  = m
+rankValue r | r == Ace = 0
+rankValue _            = 10
 
 nbrOfAces :: Hand -> Integer
 nbrOfAces Empty = 0
-nbrOfAces (Add (Card rank suit) Empty) | rank == Ace = 1
 nbrOfAces (Add (Card rank suit) hand) | rank == Ace = 1 + (nbrOfAces hand)
+nbrOfAces (Add (Card rank suit) hand) = (nbrOfAces hand)
 
 --gameOver :: Hand -> Hand -> Player
 
