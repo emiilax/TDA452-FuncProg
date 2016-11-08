0f2561a6a62157bb44fde7b9ee7f13a00961f209
Erik Karlkvist <erikkarlkvist95@gmail.com>
2016-11-08 16:37:14 +0100

    all methods work for submission 2

--- lab2/BlackJack.hs
+++ lab2/BlackJack.hs
@@ -10,28 +10,26 @@ import           RunGame
 --            = 1 + 1 + 0
 --            = 2
 --
-
+--3.1
 empty :: Hand
 empty = Empty
 
 example_card_1 = Card (Numeric 5) Clubs
-example_card_2 = Card King Spades
-example_card_3 = Card Ace Clubs
-example_card_4 = Card (Numeric 5) Diamonds
+example_card_2 = Card (Numeric 5) Diamonds
+example_card_3 = Card (Numeric 8) Clubs
 
 example_hand_1 = Add example_card_1 Empty
 example_hand_2 = Add example_card_2 example_hand_1
 example_hand_3 = Add example_card_3 example_hand_2
-example_hand_4 = Add example_card_4 example_hand_3
 
+--3.4
 value :: Hand -> Integer
 value Empty = 0
 value hand | (nbrOfAces hand) == 1 && value (removeAces hand) <11 = value (removeAces hand) + 11
            | (nbrOfAces hand) == 1 && value (removeAces hand) >=11 = value (removeAces hand) + nbrOfAces hand
            | (nbrOfAces hand) > 1 = value (removeAces hand) + nbrOfAces hand
-value (Add card hand) = cardValue card + value hand 
+value (Add card hand) = cardValue card + value hand
 
---might be unnecessery
 cardValue :: Card -> Integer
 cardValue (Card r s) = rankValue r
 
@@ -50,6 +48,11 @@ nbrOfAces Empty = 0
 nbrOfAces (Add (Card rank suit) hand) | rank == Ace = 1 + (nbrOfAces hand)
 nbrOfAces (Add (Card rank suit) hand) = (nbrOfAces hand)
 
---gameOver :: Hand -> Hand -> Player
+gameOver :: Hand -> Bool
+gameOver hand = value hand > 21
 
---winner :: Hand -> Hand -> Player
+winner :: Hand -> Hand -> Player
+winner guest bank | value guest <= value bank = Bank
+                  | gameOver guest = Bank
+                  | gameOver bank = Guest
+                  | value guest > value bank = Guest
