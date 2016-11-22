959e16cbd23b75bce6f82ed8308fcae10acfd69e
emiilax <e.axelsson@hotmail.se>
2016-11-22 18:15:16 +0100

    Finished up lab3A with comments

--- lab3/Sudoku.hs
+++ lab3/Sudoku.hs
@@ -130,7 +130,7 @@ allBlocksOk (x:xs) = isOkayBlock x && allBlocksOk xs
 
 
 
--- hands used in testing
+-- sudokus and lists used for testing
 
 example :: Sudoku
 example =
@@ -141,7 +141,7 @@ example =
     , [Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8]
     , [Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9]
     , [Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing]
-    , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing]
+    , [Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 3, Nothing,Nothing]
     , [Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing]
     , [Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]
     ]
